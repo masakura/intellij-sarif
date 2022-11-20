ed887b742eb238c50e864b5bc4406babe10b1f4f
GitHub Action <action@github.com>
2022-11-20 11:13:45 +0000

    Template cleanup

--- src/main/kotlin/com/github/masakura/intellijsarif/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/masakura/intellijsarif/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.masakura.intellijsarif.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.masakura.intellijsarif.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/masakura/intellijsarif/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/masakura/intellijsarif/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.masakura.intellijsarif.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.masakura.intellijsarif.MyBundle
 
 class MyApplicationService {
 
